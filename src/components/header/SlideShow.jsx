db1551c177136ad1d3912a32a912f49c418fc682
Sajjad Akhtar <93345058+SajjadReactDeveloper@users.noreply.github.com>
2022-05-30 11:53:03 +0500

    Update SlideShow.jsx

--- src/components/header/SlideShow.jsx
+++ src/components/header/SlideShow.jsx
@@ -1,30 +1,24 @@
-import React from "react";
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from "react-responsive-carousel";
 
 function SlideShow() {
   return (
-    <Carousel
-      showIndicators={true}
-      showThumbs={false}
-      showArrows={true}
-      showStatus={false}
-      autoPlay={true}
-      swipeable={true}
-    >
+    <Carousel showIndicators={true} showThumbs={false} showArrows={true} showStatus = {false} autoPlay = {true} swipeable = {true}>
       <div>
-        <img src="dd.jpg" alt="Parking Images" />
+        <img src="dd.jpg" alt='Parking Images' />
       </div>
       <div>
-        <img src="e.jpg" alt="Parking Images" />
+        <img src="e.jpg" alt='Parking Images' />
       </div>
       <div>
-        <img src="o.jpg" alt="Parking Images" />
+        <img src="o.jpg" alt='Parking Images' />
       </div>
       <div>
-        <img src="p.jpg" alt="Parking Images" />
+        <img src="p.jpg" alt='Parking Images' />
       </div>
     </Carousel>
   );
 }
 
-export default SlideShow;
+export default SlideShow
